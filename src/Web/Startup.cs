fa68b5a4c4b4ad24d9450d16f857466142d0cfe4
Damian Dorocicz <damiand44444@gmail.com>
2021-10-16 21:05:35 +0200

    Autofac IoC Setup

--- src/Web/Startup.cs
+++ src/Web/Startup.cs
@@ -1,10 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Autofac;
 
 namespace Web
 {
@@ -28,6 +28,12 @@ namespace Web
             {
                 configuration.RootPath = "ClientApp/build";
             });
+        }
+
+        //Autofac IoC Main Container
+        public void ConfigureContainer(ContainerBuilder builder)
+        {
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
