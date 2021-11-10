47b75c4020b8c67fe4c1458695d9c3bed4d62fe3
krajfer <mrkrajfer@gmail.com>
2021-11-10 22:56:26 +0100

    autofac for ICommandDispather

---
+++ src/Infrastructure/IoC/Modules/CQRSModule.cs
@@ -0,0 +1,22 @@
+﻿using Application.Commons.CQRS.Command;
+using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.IoC.Modules
+{
+    public class CQRSModule : Autofac.Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            var asembly = typeof(CQRSModule).GetTypeInfo().Assembly;
+            //first use of autofac be proud young padavane
+            builder.RegisterAssemblyTypes(asembly).AsClosedTypesOf(typeof(ICommandDispatcher)).InstancePerLifetimeScope();
+
+        }
+    }
+}
