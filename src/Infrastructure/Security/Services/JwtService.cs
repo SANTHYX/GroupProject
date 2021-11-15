871b084bb0e069e3fc307b3f738f522e367c75b9
krajfer <mrkrajfer@gmail.com>
2021-11-15 22:18:42 +0100

    to close errors later i will comeback with tokens

--- src/Infrastructure/Security/Services/JwtService.cs
+++ src/Infrastructure/Security/Services/JwtService.cs
@@ -12,10 +12,10 @@ namespace Infrastructure.Security.Services
          * Ustaw sekretny klucz do security w safe miejscu
          * Wygeneruj token z creedentialami
          * użyj clamesów gdzie uniqueId//UniqueName == Id użytkownika 
-         */
+        
         public (string tokenPayload, string refresh, DateTime expirationTime) CreateJWT(User user)
         {
             
-     //   }
+     } */
     }
 }
