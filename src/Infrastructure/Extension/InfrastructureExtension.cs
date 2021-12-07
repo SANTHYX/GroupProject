260a2ce7c17b106b5123f1074b7233945ce14a37
Damian Dorocicz <damiand44444@gmail.com>
2021-12-07 20:16:07 +0100

    Security Added

--- src/Infrastructure/Extension/InfrastructureExtension.cs
+++ src/Infrastructure/Extension/InfrastructureExtension.cs
@@ -2,9 +2,13 @@
 using FluentValidation.AspNetCore;
 using Infrastructure.Options;
 using Infrastructure.Persistance;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Text;
 
 namespace Infrastructure.Extension
 {
@@ -21,6 +25,27 @@ namespace Infrastructure.Extension
             });
             services.AddFluentValidation(cfg =>
                 cfg.RegisterValidatorsFromAssemblyContaining<SignUpValidator>());
+
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+               .AddJwtBearer(x =>
+               {
+                   x.TokenValidationParameters = new TokenValidationParameters
+                   {
+                       ValidateIssuer = false,
+                       ValidateAudience = false,
+                       ValidateIssuerSigningKey = true,
+                       RequireExpirationTime = true,
+                       ValidateLifetime = true,
+                       IssuerSigningKey = new SymmetricSecurityKey(
+                           Encoding.UTF8.GetBytes(configuration.GetSection("Security:Key").Value)),
+                       ClockSkew = TimeSpan.Zero
+                   };
+               });
         }
     }
 }
